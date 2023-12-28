b26fd1cbc9d130946b045aa3dd47db3f5c8206aa
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-12-28 14:01:50 -0800

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,6 +7,12 @@ export const Navbar = () => {
     <nav>
       <div className="nav-center">
         <h3>redux tolkit</h3>
+        <div className="nav-container">
+          <CartIcon />
+          <div className="amount-container">
+            <p className="total-amount">0</p>
+          </div>
+        </div>
       </div>
     </nav>
   );
