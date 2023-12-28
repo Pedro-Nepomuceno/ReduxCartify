385580faad9b13c105f2b76c3680fe0bdbf3c2f8
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-12-28 13:55:10 -0800

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,13 @@
 import { CartIcon } from "../icons";
 import { useSelector } from "react-redux";
 
-const Navbar = () => {
+export const Navbar = () => {
   const { amount } = useSelector((state) => state.cart);
+  return (
+    <nav>
+      <div className="nav-center">
+        <h3>redux tolkit</h3>
+      </div>
+    </nav>
+  );
 };
