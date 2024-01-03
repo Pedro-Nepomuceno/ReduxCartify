1fdd2c00e3393d6fbf9d5359ea672de3f5caee65
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2024-01-03 13:52:12 -0800

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import { CartIcon } from "../icons";
 import { useSelector } from "react-redux";
 
 export const Navbar = () => {
-  const { amount } = useSelector((state) => state.cart);
+  const { amount } = useSelector((store) => store.cart);
   return (
     <nav>
       <div className="nav-center">
@@ -10,7 +10,7 @@ export const Navbar = () => {
         <div className="nav-container">
           <CartIcon />
           <div className="amount-container">
-            <p className="total-amount">0</p>
+            <p className="total-amount">{amount}</p>
           </div>
         </div>
       </div>
