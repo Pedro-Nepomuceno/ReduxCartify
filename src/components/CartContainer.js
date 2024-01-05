339cb5411ad8e69660c92018c7e65fac23e09076
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2024-01-05 11:17:52 -0800

    set cart logic

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,6 +1,19 @@
 import React from "react";
 import { CartItem } from "./CartItem";
+import { UseSelector, useSelector } from "react-redux/es/hooks/useSelector";
+
 const CartContainer = () => {
+  const { CartItems, total, amount } = useSelector((store) => store.amount);
+  if (amount < 1) {
+    return (
+      <section className="cart">
+        <header>
+          <h2>Your Bag</h2>
+          <h4 className="empty cart">is empty </h4>
+        </header>
+      </section>
+    );
+  }
   return <div>CartContainer</div>;
 };
 
