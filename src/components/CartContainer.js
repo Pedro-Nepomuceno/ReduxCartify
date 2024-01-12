252de2e996c7f1c2dc0e6391a2b56c5338f21c83
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2024-01-12 13:17:46 -0800

    start remove logic

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,16 +1,19 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import cartItems from "../cartItems";
 import { CartItem } from "./CartItem";
+import { clearCart } from "../cart/cartSlice";
 
 const CartContainer = () => {
+  const dispatch = useDispatch();
+
   const { amount, total } = useSelector((store) => store.cart);
   if (amount < 1) {
     return (
       <section className="cart">
         <header>
           <h2>Your Bag</h2>
-          <h4 className="empty-cart">is empty </h4>
+          <h4 className="empty-cart">is empty</h4>
         </header>
       </section>
     );
@@ -32,7 +35,9 @@ const CartContainer = () => {
             total <span>${total}</span>
           </h4>
         </div>
-        <button className="btn clear-btn">Clear cart</button>
+        <button className="btn clear-btn" onClick={() => dispatch(clearCart())}>
+          Clear cart
+        </button>
       </footer>
     </section>
   );
