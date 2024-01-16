69cf593c4cd7908cc567fc1821d6c192d2134a48
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2024-01-16 14:03:17 -0800

    update cart totals

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import { Navbar } from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
+import { useEffect, useCallback } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { calculateTotals } from "./cart/cartSlice";
 
 function App() {
+  const { cartItems } = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
+
+  const handleCalculateTotals = useCallback(() => {
+    dispatch(calculateTotals());
+  }, [dispatch]);
+
+  useEffect(() => {
+    handleCalculateTotals();
+  }, [cartItems, handleCalculateTotals]);
+
   return (
     <main>
       <Navbar />
