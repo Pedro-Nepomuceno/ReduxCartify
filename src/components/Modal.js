d0b6f8c3123986f2f288601b797d9f780fe722ee
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2024-01-17 12:27:14 -0800

    fix style

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,10 +6,10 @@ function Modal() {
       <div className="modal">
         <h4>Remove all items from your shopping cart?</h4>
         <div className="btn-container">
-          <button type="button" className="btn-confirm-btn">
+          <button type="button" className="btn confirm-btn">
             Confirm
           </button>
-          <button type="button" className="btn-confirm-btn">
+          <button type="button" className="btn clear-btn">
             Cancel
           </button>
         </div>
